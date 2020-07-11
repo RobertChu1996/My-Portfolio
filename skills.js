0d5422238b9b95560c9ae17df2cf3da25466fac3
YU KUANG CHU <43998428+RobertChu1996@users.noreply.github.com>
2020-07-10 22:54:02 -0500

    My Portfolio Website - First Version

---
+++ skills.js
@@ -0,0 +1,23 @@
+var skills = {
+  ht: 85,
+  jq: 70,
+  ja: 65,
+  re: 70,
+  ph: 35,
+  sw: 30,
+  git: 75
+};
+
+$.each(skills, function(key, value) {
+  var skillbar = $('.' + key);
+
+  skillbar.animate(
+  {
+    width: value + "%"
+  },
+  3000,
+  function() {
+    $(".speech-bubble").fadeIn();
+  }
+  );
+});
