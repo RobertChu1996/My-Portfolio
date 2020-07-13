a0652b72897e1ee9dc1e1a21621b5a48dcd36432
YU KUANG CHU <43998428+RobertChu1996@users.noreply.github.com>
2020-07-13 03:22:39 -0500

    My Portfolio - decided version

--- mail.js
+++ mail.js
@@ -1,23 +1,23 @@
-console.log("Works");
-
 $(document).ready(function() {
-      $(".submit").click(function(event) {
-            console.log("button Clicked");
+    $(".submit").click(function(event) {
+          console.log("button Clicked");
 
 
-            var name = $(".name").val()
-            var email = $(".email").val()
-            var subject = $(".subject").val()
-            var message = $(".message").val()
-            var status = $(".status")
-            status.empty()
+          var name = $(".Name").val()
+          var email = $(".Email").val()
+          var message = $(".Message").val()
+          var status = $(".status")
+          status.empty()
 
-            if (email.length > 5 && email.includes('@') && email.include('.')) {
-              status.append( < div > Email is valid < /div>)
-              }
-              else {
-                event.preventDefault()
+          if (email.length > 5 && email.includes('@') && email.includes('.')) {
+            // status.append( <div> Email is valid </div>)
+              console.log("Email valid");
+            }
+            else {
+              event.preventDefault()
 
-                status.append( < div > Email is valid < /div>)    }
-                })
-            })
+              // status.append( < div > Email is valid < /div>)
+              console.log("Email not valid");
+ }
+              })
+          });
--- skills.js
+++ skills.js
@@ -1,23 +1,24 @@
 var skills = {
-  ht: 85,
-  jq: 70,
-  ja: 65,
-  re: 70,
-  ph: 35,
-  sw: 30,
-  git: 75
-};
-
-$.each(skills, function(key, value) {
-  var skillbar = $('.' + key);
-
-  skillbar.animate(
-  {
-    width: value + "%"
-  },
-  3000,
-  function() {
-    $(".speech-bubble").fadeIn();
-  }
-  );
-});
+    ht: 85,
+    jq: 70,
+    ja: 65,
+    re: 70,
+    ph: 35,
+    sw: 30,
+    git: 75
+  };
+  
+  $.each(skills, function(key, value) {
+    var skillbar = $('.' + key);
+  
+    skillbar.animate(
+    {
+      width: value + "%"
+    },
+    3000,
+    function() {
+      $(".speech-bubble").fadeIn();
+    }
+    );
+  });
+  
